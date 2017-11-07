a7e81a8796a545a87f07ce65b9c8842ace1f21c0
Makson <popovich.max2010@gmail.com>
2017-11-07 22:07:36 +0200

    Ya za eto ne ruchayus

--- CourseWork/FitnessCentreApp/ViewModel/AbonimentChangeViewModel.cs
+++ CourseWork/FitnessCentreApp/ViewModel/AbonimentChangeViewModel.cs
@@ -1,13 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FitnessCentreApp.Infrastructure;
+using FitnessCentreApp.Model;
+using System.Windows.Input;
 
 namespace FitnessCentreApp.ViewModel
 {
     class AbonimentChangeViewModel : ViewModelBase
     {
         //TODO Do it ViewModel and View
+        Channal channel;
+        public AbonimentChangeViewModel()
+        {
+            channel = Channal.Create();
+        }
+
+        ObservableCollection<Aboniment> _abonimentList;
+        public ObservableCollection<Aboniment> AbonimentList
+        {
+            get
+            {
+                return _abonimentList;
+            }
+            set
+            {
+                _abonimentList = value;
+
+            }
+
+        }
+        Aboniment _currentAboniment;
+        public Aboniment AbonimentInfo
+        {
+            get
+            {
+                return _currentAboniment;
+            }
+            set
+            {
+                _currentAboniment = value;
+            }
+        }
+
+        int selectedAboniment;
+        public int SelectedAboniment
+        {
+            get
+            {
+                return selectedAboniment;
+            }
+            set
+            {
+                selectedAboniment = value;
+                OnPropertyChanged("SelectedAboniment");
+                //AbonimentInfo = channel.channal.Get(AbonimentList[SelectedAboniment].abonID);
+            }
+        }
     }
 }
